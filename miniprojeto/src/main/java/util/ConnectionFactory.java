f3a04724d8374bd6723367cc1de49a18b4123c7c
Lucas Lima <lucas.limalfss@gmail.com>
2025-01-28 15:43:12 -0300

    adiciona configuração do driver JDBC e atualiza URL de conexão; inclui dependência do MySQL no pom.xml

--- miniprojeto/src/main/java/util/ConnectionFactory.java
+++ miniprojeto/src/main/java/util/ConnectionFactory.java
@@ -2,19 +2,25 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionFactory {
-    //host: emjd1.h.filess.io
-    //database: miniprojeto_doghungarm
-    //port: 3307
-    private static final String URL = "jdbc:mysql://emjd1.h.filess.io:3307/miniprojeto_doghungarm";
+    private static final String URL = "jdbc:mysql://emjd1.h.filess.io:3307/miniprojeto_doghungarm?useSSL=false&serverTimezone=UTC";
     private static final String USER = "miniprojeto_doghungarm";
     private static final String PASSWORD = "34b732d4339c7c69803ecabcd8df5640949baa5f";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Carrega o driver explicitamente
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver JDBC não encontrado!", e);
+        }
+    }
 
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new RuntimeException("Erro ao conectar ao banco de dados", e);
         }
     }
