17299620fc09dee48d0aea0bfcb30cdaba7d379b
Lucas Lima <lucas.limalfss@gmail.com>
2025-01-28 15:22:05 -0300

    atualiza configuração do projeto para usar Maven em visualização plana e altera versão do web.xml para 3.1

--- miniprojeto/src/main/java/util/ConnectionFactory.java
+++ miniprojeto/src/main/java/util/ConnectionFactory.java
@@ -4,9 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
-    private static final String URL = "jdbc:mysql://sql10.freesqldatabase.com:3306/sql10758945";
-    private static final String USER = "sql10758945";
-    private static final String PASSWORD = "aDNK1m3HRp";
+    //host: emjd1.h.filess.io
+    //database: miniprojeto_doghungarm
+    //port: 3307
+    private static final String URL = "jdbc:mysql://emjd1.h.filess.io:3307/miniprojeto_doghungarm";
+    private static final String USER = "miniprojeto_doghungarm";
+    private static final String PASSWORD = "34b732d4339c7c69803ecabcd8df5640949baa5f";
 
     public static Connection getConnection() {
         try {
